e3d842798916bfc40d0e15c8238bf59bf30d326f
lovanto <rifkylovanto@gmail.com>
2026-05-23 22:39:01 +0700

    fix sidebar

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -232,7 +232,7 @@ function Sidebar({
         data-slot="sidebar-container"
         data-side={side}
         className={cn(
-          "fixed inset-y-0 z-10 hidden h-svh w-(--sidebar-width) transition-[left,right,width,transform] duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] data-[side=left]:left-0 data-[side=left]:group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)] data-[side=right]:right-0 data-[side=right]:group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)] md:flex group-data-[collapsible=icon]:scale-[0.98] group-data-[state=expanded]:scale-100",
+          "fixed inset-y-0 z-10 hidden h-svh w-(--sidebar-width) transition-[left,right,width] duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] data-[side=left]:left-0 data-[side=left]:group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)] data-[side=right]:right-0 data-[side=right]:group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)] md:flex",
           // Adjust the padding for floating and inset variants.
           variant === "floating" || variant === "inset"
             ? "p-(--sidebar-gap) group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+var(--sidebar-gap)*2+2px)]"
